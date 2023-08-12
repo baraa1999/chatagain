3acc022dab6c783fbc7d5cb1374fb3959e127654
baraa1999 <baraaalian7@gmail.com>
2023-08-12 11:33:20 +0300

    Login States

---
+++ lib/pages/cubits/cubit/login_state.dart
@@ -0,0 +1,12 @@
+part of 'login_cubit.dart';
+
+@immutable
+sealed class LoginState {}
+
+class LoginInitial extends LoginState {}
+
+class LoginSuccess extends LoginState {}
+
+class LoginLoading extends LoginState {}
+
+class LoginFailur extends LoginState {}
