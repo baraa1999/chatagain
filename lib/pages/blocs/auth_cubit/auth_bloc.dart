37ed508be03c334ea5958085626c9b54683d95cb
baraa1999 <baraaalian7@gmail.com>
2023-08-15 10:48:48 +0300

    Creating Auth Bloc

---
+++ lib/pages/blocs/auth_cubit/auth_bloc.dart
@@ -0,0 +1,30 @@
+import 'package:bloc/bloc.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:meta/meta.dart';
+
+part 'auth_event.dart';
+part 'auth_state.dart';
+
+class AuthBloc extends Bloc<AuthEvent, AuthState> {
+  AuthBloc() : super(AuthInitial()) {
+    on<AuthEvent>((event, emit) async {
+      if (event is LoginEvent) {
+        emit(LoginLoading());
+        try {
+          UserCredential user = await FirebaseAuth.instance
+              .signInWithEmailAndPassword(
+                  email: event.email, password: event.password);
+          emit(LoginSuccess());
+        } on FirebaseAuthException catch (ex) {
+          if (ex.code == 'user-not-found') {
+            emit(LoginFailur(errMessage: 'user not found'));
+          } else if (ex.code == 'wrong-password') {
+            emit(LoginFailur(errMessage: 'wrong passwordd'));
+          }
+        } catch (e) {
+          emit(LoginFailur(errMessage: 'something with wrong'));
+        }
+      }
+    });
+  }
+}
